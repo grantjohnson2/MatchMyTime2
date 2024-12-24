b07d31037937d212d7d45c09ca7c2a4739426572
SaakshiMunot <munot.saakshi@gmail.com>
2024-12-23 23:27:33 -0500

    added some more basic components on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
     <main className="h-screen flex flex-col gap-2 justify-center items-center">
-    <Calendar></Calendar>
-    <Button>Create Event</Button>
+      <h1 className="text-3xl font-bold">Welcome to MatchMyTime!</h1>
+      <h2 className="text-xl font-semibold">Let's Create an Event</h2>
+      <Input type="text" placeholder="Event Name" className="w-60" />
+      <p>What times may work</p>
+      <div id="select-time" className="flex items-center space-x-2">
+        <input type="time" className="border rounded px-2 py-1" />
+        <p>to</p>
+        <input type="time" className="border rounded px-2 py-1" />
+      </div>
+      <p>What days may work?</p>
+      <Calendar></Calendar>
+      <Button>Create Event</Button>
     </main>
+
   );
 }
